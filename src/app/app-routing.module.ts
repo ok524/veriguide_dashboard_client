6f8ef19280eb9dda3677366df1de1d961fcf54f4
Omar <54698736+ok524@users.noreply.github.com>
2019-12-11 12:36:41 +0800

    Task 1 - Task 3: Remove unnecessary code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,20 +28,8 @@ const routes: Routes = [
 				loadChildren: () => import('app/views/pages/apps/e-commerce/e-commerce.module').then(m => m.ECommerceModule),
 			},
 			{
-				path: 'ngbootstrap',
-				loadChildren: () => import('app/views/pages/ngbootstrap/ngbootstrap.module').then(m => m.NgbootstrapModule)
-			},
-			{
-				path: 'material',
-				loadChildren: () => import('app/views/pages/material/material.module').then(m => m.MaterialModule)
-			},
-			{
 				path: 'user-management',
 				loadChildren: () => import('app/views/pages/user-management/user-management.module').then(m => m.UserManagementModule)
-			},
-			{
-				path: 'wizard',
-				loadChildren: () => import('app/views/pages/wizard/wizard.module').then(m => m.WizardModule)
 			},
 			{
 				path: 'builder',
